d42543e45f6f9783fca357baf63d346c2bd68969
StinanH <Stina.Hedman@chasacademy.se>
2024-03-25 00:47:36 +0100

    small fix to main

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
       <BrowserRouter>
         <Header/>
         <Routes>
-          <Route path ="/" element={<Home/>}/>
+          <Route path ="" element={<Home/>}/>
           <Route path ="/about" element={<About/>}/>
           <Route path ="/career" element={<Career/>}/>
           <Route path ="/projects" element={<Projects/>}/>
